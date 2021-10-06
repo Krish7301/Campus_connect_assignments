f00b5097726b3827185a406e592d319d5702ef01
Krish7301 <92020046+Krish7301@users.noreply.github.com>
2021-10-06 14:19:58 +0530

    Create Perfect no.c

---
+++ Perfect no.c
@@ -0,0 +1,32 @@
+//**C program to print all perfect numbers between 1 to n**
+
+#include <stdio.h>
+int main()
+{
+    int i, j, n, sum;
+    printf("Enter upper limit: ");
+    scanf("%d", &n);
+    printf("All Perfect numbers between 1 to %d :\n", n);
+    for(i=1; i<=n; i++)
+    {
+        sum = 0;
+        for(j=1; j<i; j++)
+        {
+            if(i % j == 0)
+            {
+                sum += j;
+            }
+        }
+        if(sum == i)
+        {
+            printf("%d, ", i);
+        }
+    }
+    return 0;
+}
+/*
+OUTPUT :
+Enter upper limit: 500
+All Perfect numbers between 1 to 500 :
+6, 28, 496,
+*/
